f3b780a3ad9576f40247dc89594a6cd51adf1c33
Zachary Folwick <zachary.folwick@rev.com>
2021-09-08 10:08:18 -0700

    added stuff

--- src/main/java/logintest/Login_Test.java
+++ src/main/java/logintest/Login_Test.java
@@ -1,3 +1,5 @@
+package logintest;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -6,12 +8,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login_Test {
 
+    public static WebDriver createDriver() {
+//        System.setProperty("webdriver.chrome.driver", "/Users/robertbolt/Desktop/Useful Tools/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        return driver;
+    }
+
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.setProperty("webdriver.chrome.driver", "/Users/robertbolt/Desktop/Useful Tools/chromedriver");
         System.out.println("Hello, World");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = createDriver();
+
         driver.get("https://www.google.com/");
 
         WebElement name = driver.findElement(By.name("q"));
---
+++ src/test/java/Login_Test.java
@@ -0,0 +1,5 @@
+import com.google.common.annotations.VisibleForTesting;
+
+public class Login_Test {
+
+}
