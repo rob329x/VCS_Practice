9a747b0b3d2fe15fc68afa72e2ea183e98c7ced8
Zachary Folwick <zachary.folwick@rev.com>
2021-09-14 13:53:42 -0700

    cleaned up imports

--- src/main/java/logintest/Login_Test.java
+++ src/main/java/logintest/Login_Test.java
@@ -1,12 +1,8 @@
 package logintest;
 
 import java.util.Scanner;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Login_Test {
 
